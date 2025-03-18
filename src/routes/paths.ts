778e9b13639c1d0addf074f747aea44f2f0ca3c4
Vladimir Revyagin <skorabeyf@gmail.com>
2025-03-18 21:32:27 +0300

    дефолтные пути

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,5 +1,7 @@
 export enum RoutePaths {
   MAIN = '/',
-  AUTH = '/auth/:type',
-  //тут есть параметр type, его будем ставить как дефолтное значение authType
+  AUTH = '/auth/:type', //тут есть параметр type, его будем ставить как дефолтное значение authType
+  SIGNOUT = 'signout',
+  SITE = 'site',
+  SITE_NEW = 'site/new',
 }
