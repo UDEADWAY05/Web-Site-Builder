09968e9fd6d6e2e029e0fb4126284c7cab0889e5
Vladimir Revyagin <skorabeyf@gmail.com>
2025-03-18 21:40:17 +0300

    feat: подключение навигации и страниц

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,9 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import { Main } from 'src/pages/main';
-import { RoutePaths } from './paths';
+import { Routes, Route } from 'react-router-dom'
+import { Main } from '../pages/Main'
+import { RoutePaths } from './paths'
+import { NavBar } from 'src/components/ui/navBar'
+import { SiteNew } from 'src/pages/SiteNew'
 
-export const AppRoutes = () => (
-    <Routes>
+export const AppRoutes = () => {
+  return (
+    <>
+      <NavBar />
+      <Routes>
         <Route path={RoutePaths.MAIN} element={<Main />} />
-    </Routes>
-);
+        <Route path={RoutePaths.SITE_NEW} element={<SiteNew />} />
+      </Routes>
+    </>
+  )
+}
