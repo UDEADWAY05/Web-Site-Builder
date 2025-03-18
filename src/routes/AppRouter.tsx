8294635dea249845a9886a764eb7c0501f515b04
Vladimir Revyagin <skorabeyf@gmail.com>
2025-03-18 22:22:00 +0300

    feat: дефолтная сраница 404

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import { Main } from '../pages/Main'
+import { Main } from '../pages/MainPage'
 import { RoutePaths } from './paths'
 import { NavBar } from 'src/components/ui/navBar'
-import { SiteNew } from 'src/pages/SiteNew'
+import { SiteNew } from 'src/pages/SiteNewPage'
+import { NotFound } from 'src/pages/NotFoundPage'
 
 export const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ export const AppRoutes = () => {
       <Routes>
         <Route path={RoutePaths.MAIN} element={<Main />} />
         <Route path={RoutePaths.SITE_NEW} element={<SiteNew />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
