bd6ade4de3712a4c48057936875c1b678faed888
igor_malykhin <malykhinig@gmail.com>
2025-03-23 13:30:06 +0700

    feat(store.ts): добавлен персистор

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,30 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist'
+import persistStore from 'redux-persist/es/persistStore'
+import storage from 'redux-persist/lib/storage'
 import userReducer from './slices/userSlice/userSlice'
 
 const rootReducer = combineReducers({
     user:userReducer
 })
 
+const persistedReducer = persistReducer(
+    { key:'redux',storage:storage },
+    rootReducer
+)
+
 export const store = configureStore({
-    reducer: rootReducer,
-    devTools: true,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat([])
+        return getDefaultMiddleware({
+            serializableCheck:{
+                ignoredActions:[FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER]
+            }
+        }).concat([])
     }
 })
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
