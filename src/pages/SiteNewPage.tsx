f1f3042075f29d8cc58ffd938392f3ec6e0ed83a
Vladimir Revyagin <skorabeyf@gmail.com>
2025-03-25 19:52:25 +0300

    feat: структура страницы

--- src/pages/SiteNewPage.tsx
+++ src/pages/SiteNewPage.tsx
@@ -1,9 +1,30 @@
-type Props = {}
+import { useState } from 'react'
+import { DraggableBlock, SideBar } from 'src/components/ui/siteNew'
+import { Blocks } from 'src/components/ui/siteNew/type/type'
 
-export const SiteNew = ({}: Props) => {
+export function SiteNew() {
+  const [projectName, setProjectName] = useState('My Project')
+  const [bgColor, setBgColor] = useState('#5C90FF')
+  const [blocks, setBlocks] = useState<Blocks[]>([])
+  console.log(blocks)
+
+  const addBlock = (type: string) => {
+    setBlocks([
+      ...blocks,
+      { id: Date.now(), type, bgColor: bgColor, title: projectName },
+    ])
+  }
+
   return (
-    <div data-testid="siteNew-page">
-      <p>Site New</p>
+    <div className="flex">
+      <SideBar
+        projectName={projectName}
+        setProjectName={setProjectName}
+        bgColor={bgColor}
+        setBgColor={setBgColor}
+        addBlock={addBlock}
+      />
+      <DraggableBlock blocks={blocks} bgColor={bgColor} />
     </div>
   )
 }
