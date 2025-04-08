00851b371eae7af993ddceee21f419891c1d8a05
Vladimir Revyagin <skorabeyf@gmail.com>
2025-04-08 18:08:18 +0300

    feat:базовая структура модального окна (без логики)

---
+++ src/components/ui/siteNew/generateCSS/generateCSS.tsx
@@ -0,0 +1,7 @@
+export function GenerateCSS() {
+  return (
+    <div className="flex flex-col p-2">
+      <h3 className="text-center">CSS</h3>
+    </div>
+  )
+}
---
+++ src/components/ui/siteNew/generateHTML/generateHTML.tsx
@@ -0,0 +1,7 @@
+export function GenerateHTML() {
+  return (
+    <div className="flex flex-col p-2">
+      <h3 className="text-center">HTML</h3>
+    </div>
+  )
+}
