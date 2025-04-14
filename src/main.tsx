69d49fb807650bf610cf7b25870eef6a0f751056
Dmitry <dmitrygerasimov09@gmail.com>
2025-04-14 15:41:03 +0300

    feat: выпилил dnd и redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
 import App from './App.tsx'
 import { Providers } from './provider'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor } from './store/store.ts'
 import './firebase.ts'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <PersistGate persistor={persistor}>
-            <Providers>
-                <App />
-            </Providers>
-        </PersistGate>
-    </StrictMode>,
+    <Providers>
+        <App />
+    </Providers>
 )
