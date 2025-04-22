c79d49703a4e0f73e82f9b9752f5302fbb5f4775
Vladimir Revyagin <skorabeyf@gmail.com>
2025-04-22 09:57:25 +0300

    style: на preview отображаются ВСЕ свойства

--- src/utils/generateCSSCode.ts
+++ src/utils/generateCSSCode.ts
@@ -10,9 +10,10 @@ export const generateCSSCode = (block: Block) => {
       width: ${
         block.type !== 'divider' ? `${block.styles?.width || 'auto'}` : '100%'
       };
-      background-color: ${block.styles?.background || '#fafafa'};
+      background-color: ${block.styles?.backgroundColor || '#fafafa'};
       font-size: ${block.styles?.fontSize || '14px'};
       font-style: ${block.styles?.fontStyle || ''};
       font-weight: ${block.styles?.fontWeight || ''};
-      text-decoration: ${block.styles?.textDecoration || ''};`
+      text-decoration: ${block.styles?.textDecoration || ''};
+      color:${block.styles?.color || '#000'};`
 }
