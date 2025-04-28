918106cf1b36d15a0d74f47cd2e7164aea8ba032
Vladimir Revyagin <skorabeyf@gmail.com>
2025-04-28 18:26:20 +0300

    feat:проверка токена при старте приложения

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ import { firebaseContext } from './contexts/firebaseContext'
 import { FirebaseService } from './services/firebaseService'
 import { firebaseConfig } from './firebase'
 import { Site } from './store/slices/siteSlice/types'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { setUser } from './store/slices/userSlice'
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
@@ -41,6 +44,19 @@ const FirebaseApiProvider = ({ children }: { children: React.ReactNode }) => {
 }
 
 function App() {
+  // проверка токена при старте приложения
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const unsub = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const token = await user.getIdToken()
+        dispatch(setUser({ email: user.email, token }))
+        localStorage.setItem('jwt-refresh-token', user.refreshToken)
+      }
+    })
+    return () => unsub()
+  }, [])
+
   return (
     <div className="App">
       <ErrorBoundary>
