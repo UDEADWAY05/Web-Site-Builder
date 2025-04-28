90918ff7de56f228ea32ccebbc27e72e453d0678
Vladimir Revyagin <skorabeyf@gmail.com>
2025-04-28 18:25:15 +0300

    feat: поправил naming

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,36 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Main } from '../pages/MainPage'
+import { RoutePaths } from './paths'
+import { ProtectedRoute } from 'src/components/ui/auth/ProtectedRoute'
+import { SiteNew } from 'src/pages/SiteNewPage'
+import { NotFound } from 'src/pages/NotFoundPage'
+import { UserPage } from 'src/pages/UserPage'
+import { AuthWrapper } from 'src/components/ui/auth/AuthWrapper'
+import { Login } from 'src/components/ui/auth/Login'
+import { SignUp } from 'src/components/ui/auth/Signup'
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* Защищенные маршруты для авторизованных пользователей */}
+      <Route element={<ProtectedRoute auth={true} />}>
+        <Route path={RoutePaths.MAIN} element={<Main />} />
+        <Route
+          path={`${RoutePaths.SITES}/:siteId?/:edit?`}
+          element={<SiteNew />}
+        />
+        <Route path={RoutePaths.USER} element={<UserPage />} />
+      </Route>
+
+      {/* Маршруты для неавторизованных пользователей */}
+      <Route element={<ProtectedRoute auth={false} />}>
+        <Route path={RoutePaths.AUTH} element={<AuthWrapper />}>
+          <Route path="" element={<Navigate to="login" replace />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
