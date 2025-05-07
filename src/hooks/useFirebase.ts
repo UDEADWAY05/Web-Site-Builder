e80652a21a68ecbe66b7417cbe7d80dfaaa55eef
Vladimir Revyagin <skorabeyf@gmail.com>
2025-05-07 13:12:11 +0300

    feat: авторизация с учетом токена

--- src/hooks/useFirebase.ts
+++
@@ -1,8 +0,0 @@
-import { firebaseContext } from "src/contexts/firebaseContext";
-import { useStrictContext } from "./useStrictContext";
-
-export const useFirebase = () => {
-    const { signIn,signUp,signOutUser,getUserById,updateUser } = useStrictContext(firebaseContext);
-
-    return { signIn, signUp, signOutUser,getUserById,updateUser };
-  };
